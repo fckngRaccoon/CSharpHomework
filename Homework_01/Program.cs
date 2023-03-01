8d71bb659f0c403dce27b256e21cfb7e0e57be56
cppdev <fckngRaccoon@yandex.ru>
2023-03-02 00:29:34 +0300

    Ex_01

---
+++ Homework_01/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите два числа: ");
+int FirstNumber = Console.Read();
+Console.Write(", ");
+int SecondNumber = Console.Read();
+Console.WriteLine(".");
+
+if (FirstNumber > SecondNumber)
+{
+    Console.Write("Наибольшее число: ");
+    Console.WriteLine(FirstNumber);
+}
+else if (FirstNumber > SecondNumber)
+{
+    Console.Write("Наибольшее число: ");
+    Console.WriteLine(SecondNumber);
+}
+else
+{
+   Console.WriteLine("Числа равны!");
+
+}
